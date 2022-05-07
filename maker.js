6d7b3e9c6352df97e9e30a92a22646520d2f067d
Jonathan Chandler <49777173+Lithovox@users.noreply.github.com>
2022-05-07 15:48:05 -0700

    Mostly working. UI needs some polish and video broken.

---
+++ maker.js
@@ -0,0 +1,101 @@
+//initialize inputs
+//set date input to today
+let today = new Date();
+let day = today.getDate();
+let month = today.getMonth() + 1;
+let year = today.getFullYear();
+if (month < 10) month = "0" + month;
+if (day < 10) day = "0" + day;    
+document.getElementById("date").value = year + "-" + month + "-" + day;
+
+//set time input default to 5 minutes from now
+document.getElementById("time").value = new Date(new Date().getTime()+60*60*1000).toTimeString().slice(0,8);
+
+//Based on user inputs on the page, generate a URL with query string to the corresponding timer page, and display a preview of that page.
+function generate() {
+    //Figure out URL path for final page
+    let path = "";
+
+    //get inputs, append to the URL string.
+
+    //get date/time by adding elements to a string.
+    let dateTime = "YYYY-MM-DDTHH:MM:SSZ"; //placeholder to remind proper format for date constructor.
+    dateTime = document.getElementById("date").value + "T"; 
+    dateTime += document.getElementById("time").value;
+    if (document.getElementById("sync").value == "sync") {
+        //if sync is slected, convert date to UTC string
+        dateTime = new Date(dateTime).toISOString();
+    } 
+    path += "date=" + encodeURIComponent(dateTime) + "&";
+
+    //add start/finish text. If blank, do not add.
+    if (document.getElementById("title").value){
+        path += "title=" + encodeURIComponent(document.getElementById("title").value) + "&";
+    }
+    if (document.getElementById("finish").value){
+        path += "finish=" + encodeURIComponent(document.getElementById("finish").value) + "&";
+    }
+
+    //add cosmetic options
+    if (document.getElementById("decimals").value < 4 && document.getElementById("decimals").value > 0) { // timer code checks for number so this is fine
+        path += "decimals=" + encodeURIComponent(document.getElementById("decimals").value) + "&";
+    }
+    if (document.getElementById("bgImg").value) {
+        path += "bgImg=" + encodeURIComponent(document.getElementById("bgImg").value) + "&";
+    }
+    if (document.getElementById("bgCol").value.toLowerCase()!=="#ffffff") { // if not default color, pass color.
+        path += "bgCol=" + encodeURIComponent(document.getElementById("bgCol").value) + "&";
+    }
+    if (document.getElementById("textCol").value!=="#000000") { // if not default color, pass color.
+        path += "textCol=" + encodeURIComponent(document.getElementById("textCol").value) + "&";
+    }
+    if (document.getElementById("finishCol").value!=="#000000") { // if not default color, pass color.
+        path += "finishCol=" + encodeURIComponent(document.getElementById("finishCol").value) + "&";
+    }
+    //the following all have default behavior if no query string is provided.
+    if (document.getElementById("shadow").checked) {
+        path += "shadowCol=" + encodeURIComponent(document.getElementById("shadowCol").value) + "&";
+    }
+    if (document.getElementById("box").checked) {
+        path += "boxCol=" + encodeURIComponent(document.getElementById("boxCol").value) + "&";
+    }
+    if (document.getElementById("underline").checked) {
+        path += "underline=1&";
+    }
+    if (document.getElementById("hideLabels").checked) {
+        path += "hideLabels=1&";
+    }
+    if (document.getElementById("hideDays").checked) {
+        path += "hideDays=1&";
+    }
+    if (document.getElementById("bgVid").value) { //youtube background video link
+        vidLink = document.getElementById("bgVid").value;
+        let match = vidLink.match(/^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=|\?v=)([^#&?]*).*/); //regex to find video ID from link
+        if (match && match[2].length == 11) {
+            path += "bgVid=" + encodeURIComponent(match[2]) + "&";
+        }
+    }
+
+    //add advanced options
+    if (document.getElementById("countUp").value=="up") {
+        path += "countUp=1&"; //Confusingly, setting countUp=1 in the url sets countUp=-1 in the timer.
+    }
+    if (document.getElementById("recur").value !== "no") {
+        path += "recur=" + document.getElementById("recur").value + "&";
+    }
+    if (document.getElementById("duration").checked) {
+        path += "duration=" + encodeURIComponent(document.getElementById("length").value) + "&";
+    }
+    if (document.getElementById("obfuscate").checked) {
+        path = "obfsc=8&" + btoa(path);
+    }
+
+    //update output URL
+    path = window.location.href.slice(0,window.location.href.lastIndexOf("/")) + "/timer.html?" + path; //replace page name with maker.html, add ? to start query string;
+
+    document.getElementById("outputURL").textContent = path; 
+    //update preview iFrame
+	document.getElementById('preview').setAttribute("src",path);
+}
+//click a generate button
+document.getElementById("generate").addEventListener("click", generate);
---
+++ timer.js
@@ -0,0 +1,228 @@
+//Displays a countdown to a given time, with cosmetic customizations based on URL query string.
+//Page argument format should be document.html?date=YYYY-MM-DDTHH:MM:SSZ&title=foo&finish=bar
+//For instance a countdown to 1 February 2034 at 5:30 p.m., with title text "The thing happens in..." and the timer reading "Now!" when it reaches 0
+//should have a query string ?date=2034-02-01T17:30:00Z&title=The thing happens in...&finish=Now!
+//Note that since a URL cannot end with a question mark you may have to add an extra dummy variable at the end in some cases; for instance use
+//the string ?date=2034-02-01T17:30:00Z&title=How long until the thing happens?& when the URL would end in a question mark otherwise.
+//If date is not populated a one minute timer is the default. If title is not populated then title is hidden by default.
+//A number of other customizations are available, explained further below.
+
+//helper code to parse URL query string
+let query = window.location.search.substring(1); //save url
+if (query.includes("obfsc=8&")) { //if url is obfuscated, de-obfuscate
+	query = atob(query.replace("obfsc=8&",""));
+}
+let vars = query.split("&");
+function getQueryVariable(variable) {
+	for (let i=0;i<vars.length;i++) {
+		let pair = vars[i].split("=");
+		if(pair[0] == variable) {
+			return decodeURIComponent(pair[1]);
+		}
+	}
+	return(false);
+}
+
+//Parsing the User settings from the query URL
+//Technically it would be more efficient to just pass all of these things to the DOM directly
+//but that would create serious content injection issues. Managing styles individually is safer.
+
+let now = new Date(); //find current time to use in calculations
+
+// Set the countdown target time. query variable name date.
+// Ideally the person entering the URL is entering a UTC time (using a Z at the end of the date string).
+// This way the countdown works the same for users anywhere; i.e. a user in one location can send the page to a user in a different timezone,
+// and the countdown should run out at the same absolute time for both of them. 
+// Local times (without the Z at the end) can still be entered but will cause different behavior in different time zones.
+let targetDate = new Date(getQueryVariable("date")).getTime();
+// if no date is selected, use the current time.
+if (isNaN(targetDate)||!getQueryVariable("date")) {
+	targetDate = now.getTime();
+}
+
+// page title. query variable name title.
+// innerText is used instead of innerHTML because since elements are populated using the URL contents there is a significant risk of content injection.
+let titleText = getQueryVariable("title");
+//if no title was supplied, make title blank
+if (!getQueryVariable("title")) {
+	titleText = "";
+}
+document.title = titleText;
+document.getElementById("title").innerText = titleText;
+
+// finish text. query variable name finish.
+// If finish text is not supplied (is false), timer simply stops at 0.
+let finishText = getQueryVariable("finish");
+if (getQueryVariable("finish") == false) {
+	finishText = false;
+}
+
+//Countup. query variable name countUp.
+//Counts up from a time in the past if enabled. -1 corresponds to true; 1 to false.
+//For instance, if you want to record "How long since...?"
+let countUp = 1
+if (!!getQueryVariable("countUp")) {
+	countUp = -1;
+	//if the date right now is in the future/no date was supplied, set timer to start counting from current time.
+	if (targetDate-now.getTime()>0) {
+		targetDate = now.getTime();
+	}
+}
+
+//fixed duration rather than target time. query variable name duration.
+//if countup, count up from <duration> seconds ago. if countdown, count down to <duration> seconds from now. Negatives get abs'd.
+let duration = 0;
+if (!isNaN(parseInt(getQueryVariable("duration")))) {
+	if (countUp == 1){//if countup false
+		targetDate = now.getTime() + Math.abs(parseInt(getQueryVariable("duration")))*1000;
+	} else {//if countup true
+		targetDate = now.getTime() - Math.abs(parseInt(getQueryVariable("duration")))*1000;
+	}
+}
+
+//recurring event. query variable name recur.
+//The timer will always count to/from the nearest relevant occurence. options are "daily", "yearly".
+//This means that the year (or the day, if applicable) supplied by the user will be ignored.
+//this option functionally does nothing if "duration" is selected.
+if (getQueryVariable("recur") == "daily") {
+	if (countUp == 1){ //if countup false (ie counting down)
+		//reschedule target to next occurrence
+		targetDate=now.getTime()+((targetDate - now.getTime()) % (1000 * 60 * 60 * 24))
+	} else { //if countup true
+		//reschedule target to previous occurrence
+		targetDate=now.getTime()-((now.getTime() - targetDate) % (1000 * 60 * 60 * 24))
+	}
+} else if (getQueryVariable("recur") == "yearly") {  
+	//due to leap years it is not sufficient to simply change the year or mod 365 days.
+	//instead set year of target to current year, then adjust one year forward or backward accordingly.
+	if (countUp == 1){ //if countup false (ie counting down)
+		//reschedule target to next occurrence
+		targetDate=targetDate.setFullYear(now.getFullYear).getTime(); //reschedule targetDate to current year
+		if (now.getTime()-targetDate>0) {//if targetDate is in the past
+			targetDate=targetDate.setFullYear(now.getFullYear + 1).getTime(); //reschedule targetDate to next year
+		}
+	} else { //if countup true
+		//reschedule target to previous occurrence
+		targetDate=targetDate.setFullYear(now.getFullYear).getTime(); //reschedule targetDate to current year
+		if (now.getTime()-targetDate<0) {//if targetDate is in the future
+			targetDate=targetDate.setFullYear(now.getFullYear - 1).getTime(); //reschedule targetDate to last year
+		}
+	}
+} 
+
+//toggle fractions of seconds. query variable name decimals.
+//the decimals variable is later used to determine how many decimal places to display.
+//a little bit of checking to make sure only integers 0-3 get used.
+let decimals = 0;
+if (!isNaN(parseInt(getQueryVariable("decimals")))) {
+	decimals = Math.max(Math.min(getQueryVariable("decimals"),3),0);
+}
+
+//background video. query variable bgVid.
+//takes a youtube link. this overrides other background options.
+//takes in youtube video identifier of form dQw4w9WgXcQ and converts to form 
+//https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0&showinfo=0&mute=1&rel=0&autoplay=1&loop=1&playlist=dQw4w9WgXcQ
+if (!!getQueryVariable("bgVid")) {
+	let embed = "https://www.youtube.com/embed/" + getQueryVariable("bgVid") + "?controls=0&showinfo=0&enablejsapi=1&mute=1&rel=0&autoplay=1&loop=1&playlist=" + getQueryVariable("bgVid") + "&origin=" + encodeURI(window.location.origin);
+	document.getElementById("backgroundVideoFrame").setAttribute("src", embed);
+	document.getElementById("backgroundVideoFrame").style.visibility = "visible";
+	console.log(embed);
+}
+
+//background image. query variable name bgImg.
+//if background image invalid the browser will fall back to background color anyways so no extra error handling here
+document.body.style.backgroundImage = "url(" + getQueryVariable("bgImg") + ")"; 
+document.body.style.backgroundSize = "cover"; 
+
+//background color. query variable name bgCol.
+//overwritten by background image if applicable. Default is white.
+document.body.style.backgroundColor = getQueryVariable("bgCol"); 
+
+//finish text color. query variable name finishCol.
+//save as a variable since finish text gets updated when the timer runs out.
+//for instance if you want the timer to turn red when time runs out.
+let finishCol = getQueryVariable("finishCol");
+
+//general text color. query variable name textCol.
+document.body.style.color = getQueryVariable("textCol");
+
+//textshadow color. query variable name shadowCol.
+//intended to improve readability with certain backgrounds. default is no textshadow.
+document.body.style.textShadow = "2px 2px " + getQueryVariable("shadowCol");
+
+//box color. query variable name boxCol.
+//puts a background box around the text to improve readability. default is transparent (no box).
+document.getElementById("box").style.backgroundColor = getQueryVariable("boxCol");
+
+//underline timer. query variable underline.
+//puts a line under the timer numbers. Intended to make reading the labels easier. default is no underline.
+if (!!getQueryVariable("underline")) {
+	Array.from(document.getElementsByClassName("time")).forEach((element) => {
+		element.style.textDecoration="0.2rem underline";
+	});
+}
+
+//show/hide time labels. query variable name hideLabels.
+//If preference is not supplied (is false), make labels visible. (default is visible)
+if (!!getQueryVariable("hideLabels")) {
+	Array.from(document.getElementsByClassName("label")).forEach((element) => {
+		element.style.display="none";
+	});
+}
+
+// show/hide days. query variable name hideDays.
+// default is show days. Note that since removing elements messes with the grid the grid is resized.
+let hideDays = !!getQueryVariable("hideDays") //cast as boolean
+if (hideDays) {
+	Array.from(document.getElementsByClassName("days")).forEach((element) => {
+		element.style.display="none";
+	});
+	document.getElementById("timer").style.gridTemplateColumns = "min-content min-content min-content min-content min-content";
+}
+
+//Main
+// Countdown display. Update every 30ms (roughly 30fps)
+var x = setInterval(function() {
+
+	// Refresh the current date and time.
+	now = new Date().getTime();
+  
+	// Find the distance between now and the count down date
+	// countUp = -1 if enabled.
+	let distance = countUp*(targetDate - now);
+    
+	// calculations for hours, minutes, seconds
+	// alter caulcation based on whether days will be displayed or everything is in hours
+	if (!hideDays) {
+		let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		document.getElementById("days").innerText = days.toString().padStart(2, "0");
+		document.getElementById("hours").innerText = hours.toString().padStart(2, "0");
+	} else {
+		let hours = Math.floor(distance/(1000 * 60 * 60));
+		document.getElementById("hours").innerText = hours.toString().padStart(2, "0");
+	}
+
+	let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	let seconds = ((distance % (1000 * 60)) / 1000);
+    
+	// Output the result in timer div
+	document.getElementById("minutes").innerText = minutes.toString().padStart(2, 0);
+	//display seconds. adds decimals as appropriate. !!casts to boolean (1 or 0).
+	document.getElementById("seconds").innerText = seconds.toFixed(decimals).toString().padStart(!!decimals+decimals+2, 0);
+    
+	// If the countdown ends, replace the timer with text
+	if (distance < 0) {
+		clearInterval(x);
+		if (finishText) {// if no finish text was supplied, just stop the timer at 0.
+			document.getElementById("timer").innerText = finishText;
+			document.getElementById("timer").style.color = finishCol;
+		} else {
+			document.getElementById("days").innerText = "00";
+			document.getElementById("hours").innerText = "00";
+			document.getElementById("minutes").innerText = "00";
+			document.getElementById("seconds").innerText = Number(0).toFixed(decimals).toString().padStart(!!decimals+decimals+2, 0);
+		}
+
+  }
+}, 30);
